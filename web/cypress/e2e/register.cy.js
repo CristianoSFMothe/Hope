ce7facdff26909bc4673e7ee5d1227937609900f
CristianoSFMothe <cristianodevsystem@gmail.com>
2025-04-30 01:10:33 -0300

    feat: implement postOrphanage command and update registration tests

--- web/cypress/e2e/register.cy.js
+++ web/cypress/e2e/register.cy.js
@@ -42,29 +42,7 @@ describe('Cadastro de orfanatos', () => {
 
     cy.deleteMany({ name: orphanage.name }, { collection: 'orphanages' })
     // Primeiro cadastro
-    cy.goto('http://localhost:3000/orphanages/create');
-
-    cy.get('legend')
-      .should('be.visible')
-      .and('have.text', 'Cadastro')
-
-    cy.setMapPosition(orphanage.position)
-
-    cy.contains('label', 'Nome')
-      .parent()
-      .find('input')
-      .type(orphanage.name)
-
-    cy.get('#description').type(orphanage.description)
-
-    cy.get('input[type=file]')
-      .selectFile("cypress/fixtures/images/kids-playground-1.png", { force: true })
-
-    cy.get('#opening_hours').type(orphanage.opening_hours)
-
-    cy.contains('button', orphanage.open_on_weekends).click()
-
-    cy.get('.save-button').should('be.visible').click()
+    cy.postOrphanage(orphanage)
 
     // Segundo cadastro
     cy.goto('http://localhost:3000/orphanages/create');
--- web/cypress/support/commands.js
+++ web/cypress/support/commands.js
@@ -20,3 +20,26 @@ Cypress.Commands.add('setMapPosition', (position) => {
   window.localStorage.setItem('hope-qa:latitude', position.latitude);
   window.localStorage.setItem('hope-qa:longitude', position.longitude);
 });
+
+Cypress.Commands.add('postOrphanage', (orphanage) => {
+
+  const formData = new FormData();
+  formData.append('name', orphanage.name);
+  formData.append('description', orphanage.description);
+  formData.append('latitude', orphanage.position.latitude);
+  formData.append('longitude', orphanage.position.longitude);
+  formData.append('opening_hours', orphanage.opening_hours);
+  formData.append('open_on_weekends', true);
+  // formData.append('images', fs.createReadStream('cypress/fixtures/images/kids-playground-1.png'));
+  cy.request({
+    method: "POST",
+    url: "http://localhost:3333/orphanages",
+    headers: {
+      "Content-type": "multipart/form-data"
+    },
+
+    body: formData,
+  }).then(response => {
+    expect(response.status).to.eq(201);
+  });
+});
