acb916d17f01729f7efe59d730dca157c136491e
CristianoSFMothe <cristianodevsystem@gmail.com>
2025-05-02 02:48:23 -0300

    chore: add dotenv configuration to manage environment variables in Cypress setup

--- web/cypress.config.js
+++ web/cypress.config.js
@@ -1,22 +1,20 @@
+require("dotenv").config();
+
 const { configurePlugin } = require("cypress-mongodb");
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "t5ndpt",
   e2e: {
     setupNodeEvents(on, config) {
       configurePlugin(on)
-
-      config.env.mongodb = {
-        uri: config.env.MONGO_URI || '',
-        database: config.env.MONGO_DATABASE || '',
-      };
-
-      return config;
     },
-    baseUrl: "http://localhost:3000",
+    baseUrl: process.env.BASE_URL,
     env: {
-      baseApi: "http://localhost:3333",
+      baseApi: process.env.BASE_API,
+      mongodb: {
+        uri: process.env.MONGO_URI,
+        database: process.env.DATABASE_NAME,
+      }
     }
   },
 });
